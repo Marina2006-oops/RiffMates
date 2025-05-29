39899f1373565da312c944bb5840ff0ede6e2059
Marina2006-oops <maarinaap@mail.ru>
2025-05-29 16:32:23 +0700

    Musician

--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from home import views as home_views
 
 urlpatterns = [
--- bands/urls.py
+++ bands/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from bands import views
 urlpatterns = [
-    path ("musician/<int:musician_id>", views.musician, name="musician")
+    path("musician/<int:musician_id>", views.musician, name="musician")
 ]
